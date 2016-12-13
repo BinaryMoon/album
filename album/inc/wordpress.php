85e46ccbd5ecc110b658bb03bdddd84f1029646f
BinaryMoon <binarymoon@gmail.com>
2016-12-13 17:56:33 +0000

    update blog post styles

--- album/inc/wordpress.php
+++ album/inc/wordpress.php
@@ -25,7 +25,7 @@
 function album_enqueue() {
 
 	// Styles.
-	wp_enqueue_style( 'album-style', get_stylesheet_uri(), null, '1.4' );
+	wp_enqueue_style( 'album-style', get_stylesheet_uri(), null, '1.5' );
 
 	// Fonts.
 	$fonts_url = album_fonts();
