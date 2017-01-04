1c263779ffa2d7064ee45da4a314df33ff2d55b3
BinaryMoon <binarymoon@gmail.com>
2017-01-04 12:16:26 +0000

    add blog title when static homepage is used

--- index.php
+++ index.php
@@ -26,6 +26,22 @@
 		<div id="main-content" class="main-content content-posts">
 
 <?php
+
+		// Display a blog title when the homepage is set to a sub page and the
+		// portfolio (or other custom page) is displayed on the homepage.
+		if ( ! is_front_page() ) {
+
+?>
+			<header class="entry-archive-header">
+
+				<h1 class="entry-title entry-archive-title"><?php esc_html_e( 'Blog', 'album' ); ?></h1>
+
+			</header>
+
+<?php
+
+		}
+
 		while ( have_posts() ) {
 
 			the_post();
