794f9773ff39994c8d4e6582c1b84890b1646f7d
BinaryMoon <binarymoon@gmail.com>
2017-01-09 09:02:36 +0000

    bump css version

--- inc/wordpress.php
+++ inc/wordpress.php
@@ -25,7 +25,7 @@
 function album_enqueue() {
 
 	// Styles.
-	wp_enqueue_style( 'album-style', get_stylesheet_uri(), null, '1.14' );
+	wp_enqueue_style( 'album-style', get_stylesheet_uri(), null, '1.15' );
 
 	// Fonts.
 	$fonts_url = album_fonts();
