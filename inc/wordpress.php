7d3ef599afda28f9a8d9c27f0a58ec3c49e234a7
BinaryMoon <binarymoon@gmail.com>
2017-01-22 10:52:26 +0000

    update post and comments nav layout to include arrows

--- inc/wordpress.php
+++ inc/wordpress.php
@@ -25,7 +25,7 @@
 function album_enqueue() {
 
 	// Styles.
-	wp_enqueue_style( 'album-style', get_stylesheet_uri(), null, '1.21' );
+	wp_enqueue_style( 'album-style', get_stylesheet_uri(), null, '1.22' );
 
 	// Fonts.
 	$fonts_url = album_fonts();
